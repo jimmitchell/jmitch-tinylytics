66cf9a78fa4282619f014fb38e722847c5c9fdc8
Jim Mitchell <jim@jimmitchell.org>
2024-02-04 16:58:02 -0800

    shortcode tweaks to return empty values if an option is not set

--- inc/user-shortcodes.php
+++ inc/user-shortcodes.php
@@ -11,26 +11,31 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 // *** Kudos shortcode
 function jmitch_tinylytics_kudos_function() {
+	$output = '';
 	$options = get_option('jmitch_tinylytics_settings');
 	$kudos = $options['display_kudos'];
 	if ($kudos) {
-		return '<button class="tinylytics_kudos" data-path="'. wp_make_link_relative(get_permalink()) .'"></button>';
+		$output = '<button class="tinylytics_kudos" data-path="'. wp_make_link_relative(get_permalink()) .'"></button>';
 	}
+	return $output;
 }
 add_shortcode('tinykudos','jmitch_tinylytics_kudos_function');
 
 // *** Hits shortcode
 function jmitch_tinylytics_hits_function() {
+	$output = '';
 	$options = get_option('jmitch_tinylytics_settings');
 	$hits = $options['display_hits'];
 	if ($hits) {
-		return '<span class="tinylytics_hits" data-path="'. wp_make_link_relative(get_permalink()) .'"></span>';
+		$output = '<span class="tinylytics_hits" data-path="'. wp_make_link_relative(get_permalink()) .'"></span>';
 	}
+	return $output;
 }
 add_shortcode('tinyhits','jmitch_tinylytics_hits_function');
 
 // *** Web ring shortcode
 function jmitch_tinylytics_webring_function() {
+	$output = '';
 	$options = get_option('jmitch_tinylytics_settings');
 	$webring = $options['display_webring'];
 	$webring_label = $options['webring_label'];
@@ -49,21 +54,23 @@ add_shortcode('tinywebring','jmitch_tinylytics_webring_function');
 
 // *** Country flags shortcode
 function jmitch_tinylytics_flags_function() {
+	$output = '';
 	$options = get_option('jmitch_tinylytics_settings');
 	$flags = $options['display_flags'];
 	if ($flags) {
-		return '<p><span class="tinylytics_countries"></span></p>';
+		$output = '<p><span class="tinylytics_countries"></span></p>';
 	}
+	return $output;
 }
 add_shortcode('tinyflags','jmitch_tinylytics_flags_function');
 
 // *** Stats shortcode
 function jmitch_tinylytics_stats_function() {
+	$output = '';
 	$options = get_option('jmitch_tinylytics_settings');
 	$stats = $options['display_stats'];
 	$hits = $options['display_hits'];
 	$uptime = $options['display_uptime'];
-	$output = '';
 	if ($stats && $hits) {
 		$output .= '<span class="tiny_counter"><a href="" target="_blank" class="tinylytics_public_stats">My Stats</a>: <span class="tinylytics_hits"></span></span>';
 	}
