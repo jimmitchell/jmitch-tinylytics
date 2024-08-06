6d4d0d59f8ce8d191183a119f53ef464735af664
Jim Mitchell <jim@jimmitchell.org>
2024-08-06 04:47:14 -0700

    update styles and change donation plea

--- inc/admin-support.php
+++ inc/admin-support.php
@@ -11,8 +11,8 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 ?>
 
-<h2 style="text-align:center;"><?php esc_html_e( 'Your Support Matters', 'jmitch-tinylytics' ); ?></h2>
+<h2 style="text-align:center;"><?php esc_html_e( 'Enjoying Tinylytics for WordPress?', 'jmitch-tinylytics' ); ?></h2>
 
-<p><?php esc_html_e( 'Though I freely provide this plugin to the WordPress community, consider making a small donation. A few bucks goes a long way towards making improvements', 'jmitch-tinylytics' ); ?>.</p>
+<p><?php esc_html_e( 'I love offering this plugin to the WordPress community for free! If you find it useful, consider supporting its development. Even a small donation can make a big difference in helping me improve and enhance the plugin for everyone. Your generosity ensures continuous updates and new features', 'jmitch-tinylytics' ); ?>.</p>
 <p><strong><?php esc_html_e( 'Note:', 'jmitch-tinylytics' ); ?></strong> <?php esc_html_e( 'Making a donation gets you priority support if you need it', 'jmitch-tinylytics' ); ?>.</p>
-<p style="margin-top:25px;"><a href="https://donate.stripe.com/9AQ8Ab6Yr8Y67cYdQR" target="_blank" class="donate-button"><?php esc_html_e( 'Support Continued Development' ); ?></a></p>
+<p style="margin-top:25px;"><a href="https://donate.stripe.com/9AQ8Ab6Yr8Y67cYdQR" target="_blank" class="donate-button button button-primary"><?php esc_html_e( 'Donate Today and Help Me Grow!' ); ?></a></p>
